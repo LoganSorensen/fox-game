2df23743b0177889e6c88b073e3fe31ef6e5d051
Logan Sorensen <loganmsorensen@gmail.com>
2020-08-11 01:42:11 -0700

    fixed day length

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const TICK_RATE = 3000;
 export const ICONS = ["fish", "poop", "weather"];
 export const RAIN_CHANCE = 0.2;
 export const SCENES = ["day", "rain"];
-export const DAY_LENGTH = 600;
+export const DAY_LENGTH = 60;
 export const NIGH_LENGTH = 4;
 
 export const getNextHungerTime = (clock) =>
